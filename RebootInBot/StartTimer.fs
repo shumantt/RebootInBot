a500a6c6f119c50c833391ef8e7e7f65f717dc91
Andrey Shumeev <shumantt@gmail.com>
2021-02-28 18:23:56 +0300

    check for cancel

--- RebootInBot/StartTimer.fs
+++ RebootInBot/StartTimer.fs
@@ -1,27 +1,33 @@
 module RebootInBot.StartTimer
 
+open System.Threading
 open RebootInBot.Types
 open RebootInBot.Mentions
 open RebootInBot.TimerProcess
 
-let private createTimerProcess updateMessage =
+let private createTimerProcess updateMessage checkIsCancelled =
     let mutable numberOfUpdates = 10
+    use cts = new CancellationTokenSource()
     let (task, timerTicks) = createTimer 1 numberOfUpdates
+
     timerTicks
-        |> Observable.subscribe (fun _ ->
-            updateMessage (sprintf "Перезапуск через %i" numberOfUpdates)
-            numberOfUpdates <- numberOfUpdates - 1)
-        |> ignore
-    task
+    |> Observable.subscribe (fun _ ->
+        updateMessage (sprintf "Перезапуск через %i" numberOfUpdates)
+        numberOfUpdates <- numberOfUpdates - 1
+        if checkIsCancelled () then cts.Cancel())
+    |> ignore
 
-let buildStartTimer getParticipants (message:IncomingMessage) =
-        { Chat = message.Chat
-          Starter = message.Author
-          ChatParticipants = getParticipants message.Chat }
+    async { Async.Start(task, cts.Token) }
 
-let processStartTimer sendMessage updateMessage startTimerCommand =
-        buildMentionList startTimerCommand.Starter startTimerCommand.ChatParticipants
-            |> sendMessage startTimerCommand.Chat
-            |> fun messageId ->
-                let updateMessage = updateMessage startTimerCommand.Chat messageId
-                createTimerProcess updateMessage
+let buildStartTimer getParticipants (message: IncomingMessage) =
+    { Chat = message.Chat
+      Starter = message.Author
+      ChatParticipants = getParticipants message.Chat }
+
+let processStartTimer sendMessage updateMessage checkIsCancelled startTimerCommand =
+    buildMentionList startTimerCommand.Starter startTimerCommand.ChatParticipants
+    |> sendMessage startTimerCommand.Chat
+    |> fun messageId ->
+        let updateMessage = updateMessage startTimerCommand.Chat messageId
+        let checkIsCancelled = checkIsCancelled startTimerCommand.Chat
+        createTimerProcess updateMessage checkIsCancelled
