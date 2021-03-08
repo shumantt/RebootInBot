c95e1004b669d6a55555f0f36dc7735e6e9eae4c
Andrey Shumeev <shumantt@gmail.com>
2021-03-08 18:48:53 +0300

    remove old code

--- RebootInBot.Processing/Constants.fs
+++
@@ -1,9 +0,0 @@
-module RebootInBot.Processing.Constants
-
-let startingCountTemplate = Printf.StringFormat<string->string>("Перезапускаю %s. Никто не против?")
-let noMachineStartCount = "Начинаю перезапуск. Никто не против?"
-let processAlreadyRunning = "Процесс перезапуска уже запущен, попробуйте позже"
-let cancelProcessMessage = "Стоп стоп стоп. Кто-то еще работает"
-let errorCancelMessage = "Отсчет не начат или уже завершен. Отменять нечего"
-let excludedMentions = "Успешно исключены из списка"
-let includeMentions = "Уведомления включены"
