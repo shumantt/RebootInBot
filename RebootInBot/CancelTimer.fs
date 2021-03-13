a9046114aa779f51abaa104eeb65ddfae980b5f8
Andrey Shumeev <shumantt@gmail.com>
2021-03-13 17:18:00 +0300

    new start draft

--- RebootInBot/CancelTimer.fs
+++ RebootInBot/CancelTimer.fs
@@ -28,16 +28,16 @@ type Timer =
     | RunningTimer of RunningTimer
     | InactiveTimer of InactiveTimer
 
-type CancelledTimer = { Timer: RunningTimer }
+type TimerCancelled = { Timer: InactiveTimer }
 
-type CancelError = { Message: string }
+type GeneralError = { Message: string }
 
 
-type FailedTimerCancellation = { Timer: Timer }
+type TimerCancellationFailure = { Timer: Timer }
 
-type CancelTimer = RunningTimer -> Async<Result<RunningTimer, CancelError>>
+type CancelTimer = RunningTimer -> Async<Result<InactiveTimer, GeneralError>>
 
-type CancelProcess = Timer -> CancelTimer -> Async<Result<CancelledTimer, FailedTimerCancellation>>
+type CancelProcess = Timer -> CancelTimer -> Async<Result<TimerCancelled, TimerCancellationFailure>>
 
 let cancelProcess: CancelProcess =
     fun timer cancelTimer ->
@@ -47,7 +47,37 @@ let cancelProcess: CancelProcess =
                 let! cancelResult = cancelTimer runningTimer
 
                 match cancelResult with
-                | Ok runningTimer -> return Result.Ok { Timer = runningTimer }
+                | Ok inactiveTimer -> return Result.Ok { Timer = inactiveTimer }
                 | Error _ -> return Result.Error { Timer = timer }
             | InactiveTimer _ -> return Result.Error { Timer = timer }
+        }
+
+
+type TimerStarted = {
+    Timer: RunningTimer
+}
+
+type TimerStartFailure = {
+    Timer: Timer
+}
+
+type StartTimerCountDownFailure =
+    | NewThrottled
+    | StartError of GeneralError
+
+type StartTimerCountDown = InactiveTimer -> Async<Result<RunningTimer, StartTimerCountDownFailure>>
+
+type StartTimerProcess = Timer -> StartTimerCountDown -> Async<Result<TimerStarted, TimerStartFailure>>
+
+
+let startTimerProcess: StartTimerProcess =
+    fun timer startTimerCountDown ->
+        async {
+            match timer with
+            | InactiveTimer inactiveTimer ->
+                let! startResult = startTimerCountDown inactiveTimer
+                match startResult with
+                | Ok runningTimer -> return Result.Ok { Timer = runningTimer }
+                | Error startCountDownFailure -> return Result.Error { Timer = timer }
+            | RunningTimer runningTimer -> return Result.Error { Timer = timer }
         }
